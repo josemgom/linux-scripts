fcae451ddf147128b99057e55646853d3d5f3150
José Manuel González Maldonado <jmg@athento.com>
2015-03-07 02:05:44 +0100

    New messages

--- nagios-slack/slack_bt_nagios.sh
+++ nagios-slack/slack_bt_nagios.sh
@@ -3,6 +3,10 @@
 # VERSION:
 # AUTHOR: Jose Manuel Gonzalez Maldonado
 
+WEBHOOK="https://hooks.slack.com/services/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"
+ICON=":bell:"
+TITLE="New alert in ${NAGIOS_HOSTNAME}"
+MESSAGE="Service: ${NAGIOS_SERVICEDISPLAYNAME}\n Alert: ${NAGIOS_SERVICEOUTPUT}"
 
 if [ "$NAGIOS_SERVICESTATE" = "CRITICAL" ]; then
     COLOR="#FF0000"
@@ -10,10 +14,10 @@ elif [ "$NAGIOS_SERVICESTATE" = "WARNING" ]; then
     COLOR="#FFF300"
 elif [ "$NAGIOS_SERVICESTATE" = "OK" ]; then
     COLOR="#2FFF00"
+else
+    COLOR="#000000"
 fi
 
-MESSAGE="Host: ${NAGIOS_HOSTNAME}\n Service: ${NAGIOS_SERVICEDISPLAYNAME}\n Alert: ${NAGIOS_SERVICEOUTPUT}",
+JSON='payload={"channel": "#general", "username": "nagios","icon_emoji": "'"$ICON"'", "color": "'"$COLOR"'", "pretext": "'"$TITLE"'", "short": true, "text": "Aqui el mensaje de la alerta"}'
 
-curl -X POST --data-urlencode 'payload={"channel": "#nagios", "username": "nagios", "color": "${COLOR}", "text": "$MESSAGE", "icon_emoji": ":ghost:"}' https://hooks.slack.com/services/<token>
-
-
+curl -X POST --data-urlencode "$JSON" $WEBHOOK
